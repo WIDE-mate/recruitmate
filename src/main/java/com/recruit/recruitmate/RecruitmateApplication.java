22664c1b05b4eaf0f6334c6016f184675908ed38
김소영 <soyoungdev.7@gmail.com>
2023-04-06 17:19:01 +0900

    first commit

---
+++ src/main/java/com/recruit/recruitmate/RecruitmateApplication.java
@@ -0,0 +1,13 @@
+package com.recruit.recruitmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmateApplication.class, args);
+	}
+
+}
