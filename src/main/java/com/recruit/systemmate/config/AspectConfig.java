b8ee96caa90a5cdb19b12a87cadc253a64329400
SonGwangho <SonGwangho>
2023-04-14 08:51:59 +0900

    new file:   src/main/java/com/recruit/systemmate/config/AspectConfig.java

---
+++ src/main/java/com/recruit/systemmate/config/AspectConfig.java
@@ -0,0 +1,33 @@
+package com.recruit.systemmate.config;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AspectConfig {
+    
+    @Around("execution(* com.recruit.recruitmate..controller.*.*(..)")
+    public Object aroundRecruitmateController(ProceedingJoinPoint joinPoint){
+        Object[] args = joinPoint.getArgs();
+        HttpServletRequest request = null;
+        HttpServletResponse response = null;
+
+        for(Object obj : args){
+            if(obj instanceof HttpServletRequest){
+                request = (HttpServletRequest) obj;
+            }else if(obj instanceof HttpServletRequest){
+                response = (HttpServletResponse) obj;
+            }
+        }
+
+        return null;
+    }
+}
