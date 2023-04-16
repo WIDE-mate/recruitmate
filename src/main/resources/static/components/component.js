7cdceb4595fe1b63d0e3831d9228873faf400202
SonGwangho <SonGwangho>
2023-04-17 08:54:53 +0900

    로그인에 모달 추가

--- src/main/resources/static/components/component.js
+++ src/main/resources/static/components/component.js
@@ -1,1 +1,19 @@
-<script type="text/javascript" src="./modal.js"></script>
+
+function insertComponent(components){
+    let head = document.querySelector("head");
+    for(let component of components){
+        const script = document.createElement("script");
+        script.setAttribute("type", "text/javascript");
+        script.setAttribute("src", "../static/components/" + component);
+        console.log(script)
+        head.appendChild(script)
+    }
+};
+
+(function(){
+    let components = []
+    components.push("modal.js")
+
+    insertComponent(components);
+})();
+
--- src/main/resources/static/components/modal.js
+++ src/main/resources/static/components/modal.js
@@ -2,22 +2,39 @@ class MyModal extends HTMLElement{
     constructor(){
         super();
 
+        this.classList.add("hidden");
+
         const modal_back = document.createElement('div');
-        modal_back.classList.add("modal_back", "hidden");
+        modal_back.classList.add("modal_back");
         this.appendChild(modal_back);
 
         const modal_body = document.createElement('div');
         modal_body.classList.add("modal_body");
         modal_back.appendChild(modal_body);
+
+        modal_back.addEventListener("click", (e)=>{
+            e.preventDefault();
+            let attr = e.target.parentElement.getAttribute("loading")
+            if(attr == null || attr.toString() != "true"){
+                this.classList.add('hidden')
+            }
+        })
     }
     static get observedAttributes(){ 
-        return ["loding"]
+        return ["loading"]
     }
     attributeChangedCallback(name, oldValue, newValue){
         if(name === "loading"){
             const modal_body = this.querySelector('.modal_body');
             if(newValue){
-                //로딩모양 구현해야함 귀찮음 나중에 하기
+                const loading_text = document.createElement("div")
+                loading_text.classList.add("loading_text")
+                loading_text.innerText = "loading..."
+                modal_body.appendChild(loading_text)
+
+                const loading = document.createElement("div")
+                loading.classList.add("loading")
+                modal_body.appendChild(loading)
             }else{
                 while(modal_body.hasChildNodes()){
                     modal_body.removeChild(modal_body.childNodes[0])
@@ -34,6 +51,7 @@ class MyModal extends HTMLElement{
     }
     loading(){
         this.setAttribute("loading", true)
+        this.show()
     }
     message(text){
         this.setAttribute("loading", false)
@@ -43,6 +61,3 @@ class MyModal extends HTMLElement{
     }
 }
 customElements.define("my-modal", MyModal);
-(()=>{
-    document.body.appendChild(document.createElement('my-modal'));
-})();
