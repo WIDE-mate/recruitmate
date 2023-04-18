f7d3906f98a5b5e5f3b18eb4a91b2bf0d39af3cd
SonGwangho <SonGwangho>
2023-04-18 15:16:37 +0900

    리소스 위치 추가

--- src/main/java/com/recruit/systemmate/config/WebConfig.java
+++ src/main/java/com/recruit/systemmate/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.recruit.systemmate.config.auth.LoginArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,6 +18,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(loginResolver);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
     }
 
 
