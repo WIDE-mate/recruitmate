ba510da12ed53fabbda5dd84e7cf110c525696bb
김소영 <soyoungdev.7@gmail.com>
2023-05-10 17:57:59 +0900

    soeyong recruit + DB 암호화

--- src/main/java/com/recruit/configmate/AppConfig.java
+++ src/main/java/com/recruit/configmate/AppConfig.java
@@ -5,6 +5,9 @@ import com.recruit.commonmate.enums.Gender;
 import com.recruit.commonmate.enums.Grade;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -23,6 +26,22 @@ public class AppConfig {
         enumFactory.put("GENDER", Gender.class);
         enumFactory.put("GRADE", Grade.class);
         return enumFactory;
+    }
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(){
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        config.setPassword(System.getProperty("jasypt.encryptor.password"));
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setKeyObtentionIterations("1000");
+        config.setPoolSize("1");
+        config.setProviderName("SunJCE");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
     }
 
 }
